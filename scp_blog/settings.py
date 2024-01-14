db6479c1799f4b45a31e786e4d4164a4954cf08e
Kauan Carolino <kauancarolino248@gmail.com>
2024-01-14 18:31:03 -0300

    bugs corrigidos

--- scp_blog/settings.py
+++ scp_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DEBUG') == '1' else False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
